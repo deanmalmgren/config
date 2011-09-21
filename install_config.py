3e0f86d50bb07275ca7c5dfce649b4ae7a9caf32
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2011-09-21 07:29:54 -0500

    added install_config.py script to make it easy to quickly install configuration on new computers

---
+++ install_config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+"""Go through all of the files in the root of the config repository
+and add them as dotted soft links. e.g., 
+
+    ~/.bashrc -> bashrc
+"""
+
+import glob
+import os
+import sys
+
+if __name__=="__main__":
+
+    this_filename = os.path.abspath(__file__)
+    filenames = glob.iglob(os.path.join(os.path.dirname(this_filename),"*"))
+    for filename in filenames:
+        if filename != this_filename and not filename.endswith("~"):
+            linkname = os.path.expanduser(
+                os.path.join("~", '.' + os.path.basename(filename))
+            )
+            if os.path.exists(linkname):
+                os.remove(linkname)
+            os.symlink(filename, linkname)
+            
+            sys.stderr.write("ln -s %s %s\n"%(filename, linkname))
+                
+            
