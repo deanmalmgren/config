6baee6336cd4bc3cebffe3432e21c30fbd7fcbcd
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2011-10-12 13:10:56 -0500

    improved install_config.py script to handle other files as well (like .ssh/config)

--- install_config.py
+++ install_config.py
@@ -11,22 +11,35 @@ import os
 import sys
 import shutil
 
+def replace_with_softlink(filename, linkname):
+    """create softline and remove if it already exists
+    """
+    if os.path.exists(linkname):
+        try:
+            os.remove(linkname)
+        except OSError:
+            shutil.rmtree(linkname)
+    os.symlink(filename, linkname)
+
+    sys.stderr.write("ln -s %s %s\n"%(filename, linkname))
+
+
 if __name__=="__main__":
 
     this_filename = os.path.abspath(__file__)
-    filenames = glob.iglob(os.path.join(os.path.dirname(this_filename),"*"))
+    this_dirname = os.path.dirname(this_filename)
+    filenames = glob.iglob(os.path.join(this_dirname,"*"))
     for filename in filenames:
         if filename != this_filename and not filename.endswith("~"):
             linkname = os.path.expanduser(
                 os.path.join("~", '.' + os.path.basename(filename))
             )
-            if os.path.exists(linkname):
-                try:
-                    os.remove(linkname)
-                except OSError:
-                    shutil.rmtree(linkname)
-            os.symlink(filename, linkname)
-            
-            sys.stderr.write("ln -s %s %s\n"%(filename, linkname))
-                
-            
+            replace_with_softlink(filename, linkname)
+
+    # create soft links for other files
+    filenames = (
+        os.path.join(".ssh", "config"),
+    )
+    for filename in filenames:
+        linkname = os.path.expanduser(os.path.join("~", filename))
+        replace_with_softlink(os.path.join(this_dirname, filename), linkname)
